8fa3ae14ccdbf93f23a2f560ac0673fa230d9c1a
Merrelltejero21 <merrelltejero21>
2023-04-27 01:04:23 +0800

    product category finished

--- System_Project.c
+++ System_Project.c
@@ -38,7 +38,7 @@ int menus(int menu)
     int op;
     switch (menu)
         {
-            case 1:// main menu 
+            case 1:// main menu
                 system("cls");
                 printf("\tFurniture-Sales-and-Inventory-System\n\n");
                 printf("\tMAIN MENU\n\n");
@@ -53,7 +53,7 @@ int menus(int menu)
                 return op;
             break;
 
-            case 2: // item_menu 
+            case 2: // item_menu
                 system("cls");
                 printf("\tFurniture-Sales-and-Inventory-System\n\n");
                 printf("\tITEM MENU\n\n");
@@ -65,7 +65,7 @@ int menus(int menu)
                 return op;
             break;
 
-            case 3: // customer_menu 
+            case 3: // customer_menu
                 system("cls");
                 printf("\tFurniture-Sales-and-Inventory-System\n\n");
                 printf("\tCUSTOMER MENU\n\n");
@@ -78,7 +78,7 @@ int menus(int menu)
                 return op;
             break;
 
-            case 4: // sales_menu 
+            case 4: // sales_menu
                 system("cls");
                 printf("\tFurniture-Sales-and-Inventory-System\n\n");
                 printf("\tSALES MENU\n\n");
@@ -102,7 +102,7 @@ int menus(int menu)
                 return op;
             break;
 
-            case 6: // reports_and_alerts_menu 
+            case 6: // reports_and_alerts_menu
                 system("cls");
                 printf("\tFurniture-Sales-and-Inventory-System\n\n");
                 printf("\tREPORTS AND ALERTS MENU\n\n");
@@ -123,19 +123,231 @@ void item_menu()
     {
         switch (menus(2))
         {
-            case 1: 
-                printf("\t\t1\n"); system("pause");
+            case 1:
+                product_category();
             break;
 
-            case 2: 
+            case 2:
                 printf("\t\t2\n"); system("pause");
             break;
 
-            case 3: 
+            case 3:
                 return;
             break;
-            
+
             default:printf("\nSelect 1-3 ONLY!\n"); system("pause");
+        }
+    }
+}
+
+void product_category()
+{
+    int op,ch;
+
+    system("cls");
+    printf("\tFurniture-Sales-and-Inventory-System\n\n");
+    printf("\t\tPRODUCT CATEGORY\n\n");
+    printf("\t1.) Bedroom Category\n");
+    printf("\t2.) Living room Category\n");
+    printf("\t3.) Dining room Category\n");
+    printf("\t4.) Exit\n");
+    printf("\tSelect:");
+    scanf("%d",&op);
+    while (1)
+    {
+        switch (op)
+        {
+            while(1){
+            case 1:
+                system("cls");
+                printf("\t\t\n");
+                printf("\t\tBEDROOM FURNITURES\n\n\n");
+                printf("\t1.) Cabinets\n");
+                printf("\t2.) Study Table\n");
+                printf("\t3.) Bed Frame\n");
+                printf("\t4.) Vanity Mirror\n");
+                printf("\t5.) Return to selection\n");
+                printf("\tSelect: ");
+                scanf("%d",&ch);
+                system("pause");
+
+                switch(ch)
+                {
+                    case 1:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Cabinet\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 2:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Study Table\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 3:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Bed Frame\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 4:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Vanity Mirror\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 5:
+                        return;
+                        break;
+                    default:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tInvalid Input\n");
+                        printf("\n");
+                        system("pause");
+                        break;
+                }
+            }
+            break;
+            case 2:
+                while(1){
+
+                system("cls");
+                printf("\t\t\n");
+                printf("\t\tLIVING ROOM FURNITURES\n\n\n");
+                printf("\t1.) Sofa\n");
+                printf("\t2.) Tv Stand\n");
+                printf("\t3.) Table\n");
+                printf("\t4.) Bookshelf\n");
+                printf("\t5.) Return to selection\n");
+                printf("\tSelect: ");
+                scanf("%d",&ch);
+                system("pause");
+
+                switch(ch)
+                {
+                    case 1:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Sofa\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 2:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Tv Stand\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 3:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Table\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 4:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Bookshelf\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 5:
+                        return;
+                        break;
+                    default:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tInvalid Input\n");
+                        printf("\n");
+                        system("pause");
+                        break;
+                }
+            }
+            break;
+            case 3:
+                while(1){
+
+                system("cls");
+                printf("\t\t\n");
+                printf("\t\tDINING ROOM FURNITURES\n\n\n");
+                printf("\t1.) Dining Table\n");
+                printf("\t2.) Bar Carts\n");
+                printf("\t3.) Pantry Cabinets\n");
+                printf("\t4.) Bar Stools\n");
+                printf("\t5.) Return to selection\n");
+                printf("\tSelect: ");
+                scanf("%d",&ch);
+                system("pause");
+
+                switch(ch)
+                {
+                    case 1:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Cabinet\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 2:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Study Table\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 3:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Bed Frame\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    case 4:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tSelected Vanity Mirror\n");
+                        printf("\tInput Quantity: ");
+                        printf("\n");
+                        system("pause");
+                        break;
+                    default:
+                        system("cls");
+                        printf("\t\t\n");
+                        printf("\tInvalid Input\n");
+                        printf("\n");
+                        system("pause");
+                        return;
+                        break;
+                }
+            }
+            break;
+            case 4:
+                return;
+                break;
+            default:
+                printf("\t\n1-4 ONLY\n");
+                system("pause");
+                return;
+                break;
         }
     }
 }
@@ -146,22 +358,22 @@ void customer_menu()
     {
         switch (menus(3))
         {
-            case 1: 
+            case 1:
                 printf("\t\t1\n"); system("pause");
             break;
 
-            case 2: 
+            case 2:
                 printf("\t\t2\n"); system("pause");
             break;
 
-            case 3: 
+            case 3:
                 printf("\t\t3\n"); system("pause");
             break;
 
-            case 4: 
+            case 4:
                 return;
             break;
-            
+
             default:printf("\nSelect 1-4 ONLY!\n"); system("pause");
         }
     }
@@ -173,18 +385,18 @@ void sales_menu()
     {
         switch (menus(4))
         {
-            case 1: 
+            case 1:
                 printf("\t\t1\n"); system("pause");
             break;
 
-            case 2: 
+            case 2:
                 printf("\t\t2\n"); system("pause");
             break;
 
-            case 3: 
+            case 3:
                 return;
             break;
-            
+
             default:printf("\nSelect 1-3 ONLY!\n"); system("pause");
         }
     }
@@ -196,18 +408,18 @@ void receiving_items_menu()
     {
         switch (menus(5))
         {
-            case 1: 
+            case 1:
                 printf("\t\t1\n"); system("pause");
             break;
 
-            case 2: 
+            case 2:
                 printf("\t\t2\n"); system("pause");
             break;
 
-            case 3: 
+            case 3:
                 return;
             break;
-            
+
             default:printf("\nSelect 1-3 ONLY!\n"); system("pause");
         }
     }
@@ -219,22 +431,22 @@ void reports_and_alerts_menu()
     {
         switch (menus(6))
         {
-            case 1: 
+            case 1:
                 printf("\t\t1\n"); system("pause");
             break;
 
-            case 2: 
+            case 2:
                 printf("\t\t2\n"); system("pause");
             break;
 
-            case 3: 
+            case 3:
                 printf("\t\t3\n"); system("pause");
             break;
 
-            case 4: 
+            case 4:
                 return;
             break;
-            
+
             default:printf("\nSelect 1-4 ONLY!\n"); system("pause");
         }
     }
